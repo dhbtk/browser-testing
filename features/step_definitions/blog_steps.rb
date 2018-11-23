949c88453cc6e561cab247a3828c7bbd1ae0b4b7
Eduardo Antonio Niehues <eduardo@edanni.io>
2018-11-23 14:03:23 -0200

    Variable names

--- features/step_definitions/blog_steps.rb
+++ features/step_definitions/blog_steps.rb
@@ -10,8 +10,8 @@ When(/^visiting the post page$/) do
   visit post_path(@post)
 end
 
-And(/^the text "([^"]*)" must be showing$/) do |arg|
-  page.has_content? arg
+And(/^the text "([^"]*)" must be showing$/) do |text|
+  page.has_content? text
 end
 
 And(/^the post ([^ ]*) must be showing$/) do |method|
@@ -22,10 +22,10 @@ And(/^clicking the link "([^"]*)"$/) do |text|
   click_link text
 end
 
-And(/^filling the field "([^"]*)" with "([^"]*)"$/) do |arg1, arg2|
-  fill_in arg1, with: arg2
+And(/^filling the field "([^"]*)" with "([^"]*)"$/) do |label, text|
+  fill_in label, with: text
 end
 
-And(/^clicking the button "([^"]*)"$/) do |arg|
-  click_button arg
+And(/^clicking the button "([^"]*)"$/) do |name|
+  click_button name
 end
